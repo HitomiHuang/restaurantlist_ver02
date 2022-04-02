62cf727c1a285c958835fe8a09be6dbb1ee713c0
HitomiHuang <black011602@gmail.com>
2022-04-02 15:22:31 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const session = require('express-session')
 const usePassport = require('./config/passport')
 const app = express()
 const port = 3000
+const flash = require('connect-flash')
 
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
@@ -21,9 +22,12 @@ app.use(session({
   saveUninitialized: true
 }))
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 app.use(express.urlencoded({ extended:true }))
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -4,6 +4,7 @@ const User = require('../../models/user')
 const passport = require('passport')
 
 
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
@@ -19,11 +20,22 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+
+  if (!email || !password || !confirmPassword) {
+    errors.push({ message: '請填寫必填欄位。'})
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符。'})
+  }
+  if (errors.length) {
+    return res.render('register', { name, email, password, confirmPassword, errors })
+  }
 
   User.findOne({ email })
     .then(user => {
       if (user) {
-        console.log('此E-mail已註冊過。')
+        errors.push({ message: '此E-mail已註冊過。'})
         return res.render('register', { name, email, password, confirmPassword })
       } else {
         return User.create({ name, email, password, confirmPassword })
@@ -39,6 +51,7 @@ router.post('/register', (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已成功登出。')
   res.redirect('/users/login')
 })
 
